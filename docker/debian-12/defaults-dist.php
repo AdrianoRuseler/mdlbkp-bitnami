e6a5625537490fa581a15437d76a98fd8ae0b007
Adriano Ruseler <adrianoruseler@gmail.com>
2025-04-09 19:40:32 -0300

    Update defaults-dist.php

--- docker/debian-12/defaults-dist.php
+++ docker/debian-12/defaults-dist.php
@@ -19,3 +19,6 @@ $defaults['moodle']['pathtopython'] = '/usr/bin/python3';
 $defaults['moodle']['auth_instructions'] = 'Usuário: user
 Senha: bitnami';
 $defaults['moodle']['forcelogin'] = 1;
+
+$defaults['moodle']['enablewebservices'] = 1;
+$defaults['moodle']['enablewsdocumentation'] = 1;
