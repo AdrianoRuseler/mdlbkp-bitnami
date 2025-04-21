bd0662974b914bcb530ea49d528cd66a4cbffcd7
Adriano Ruseler <adrianoruseler@gmail.com>
2025-04-21 14:12:03 -0300

    Create defaults-dist.php

---
+++ docker/defaults-dist.php
@@ -0,0 +1,22 @@
+<?php
+$defaults['moodle']['summary'] = 'Moodle Backup Site! '; // for core settings
+$defaults['moodle']['custommenuitems'] = 'Theme
+-Boost | ?theme=boost
+-Classic | ?theme=classic
+';
+$defaults['moodle']['allowthemechangeonurl'] = 1; 
+$defaults['moodle']['timezone'] = 'America/Sao_Paulo';
+$defaults['moodle']['defaultcity'] = 'Curitiba';
+$defaults['moodle']['country'] = 'Brazil';
+
+$defaults['moodle']['pathtophp'] = '/opt/bitnami/php/bin/php';
+$defaults['moodle']['pathtodu'] = '/usr/bin/du';
+$defaults['moodle']['aspellpath'] = '/usr/bin/aspell';
+$defaults['moodle']['pathtogs'] = '/usr/bin/gs';
+$defaults['moodle']['pathtodot'] = '/usr/bin/dot';
+$defaults['moodle']['pathtopdftoppm'] = '/usr/bin/pdftoppm';
+$defaults['moodle']['pathtopython'] = '/usr/bin/python3';
+
+$defaults['moodle']['forcelogin'] = 1;
+$defaults['moodle']['enablewebservices'] = 1;
+$defaults['moodle']['enablewsdocumentation'] = 1;
