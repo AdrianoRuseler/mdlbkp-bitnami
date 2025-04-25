942e433c93d8fcb78ce6ed8db918ff6f97c82f36
Adriano Ruseler <adrianoruseler@gmail.com>
2025-04-25 18:48:16 -0300

    Update GenerateUsersLetters.py

--- python/GenerateUsersLetters.py
+++ python/GenerateUsersLetters.py
@@ -11,17 +11,17 @@ import random
 # username;firstname;lastname;email;password
 # Optional user fields
 # institution,department,city,country,lang,auth,timezone,idnumber,icq,phone1,phone2,address,url,description,mailformat,maildisplay,maildigest,htmleditor,autosubscribe,interests,theme
-fields = ['username', 'firstname', 'lastname', 'email','password','cohort1','idnumber'] 
+fields = ['username', 'firstname', 'lastname', 'email','password','cohort1'] 
         
 # name of csv file 
 filename = "tsamg_letters_import.csv"
 # groups = ["Student", "Teacher", "Admin"]
-gcohort = ["Estudantes", "Professores", "Administradores", "Gerentes"]
-groups = ["e","p","a","g"] 
-nusersgroup= [10,5,1,1] # number os users per group
+gcohort = ["Estudantes", "Professores","Moderadores", "Administradores", "Gerentes"]
+groups = ["e","p","m","a","g"] 
+nusersgroup= [20,10,10,2,5] # number os users per group
 Faker.seed(0)
-gpass = [fake.password(),fake.password(),fake.password(),fake.password()] # Same password per group
-gmail = ["estudante.local", "professor.local", "admin.local", "gerente.local"]
+gpass = [fake.password(),fake.password(),fake.password(),fake.password(),fake.password()] # Same password per group
+gmail = ["estudante.local", "professor.local", "moderador.local", "admin.local", "gerente.local"]
 
 # writing to csv file 
 with open(filename, 'w',newline='\n') as csvfile: 
@@ -35,12 +35,12 @@ with open(filename, 'w',newline='\n') as csvfile:
         nusers=nusersgroup[n] # Number of users in group
         gname=groups[n] # Group name
         for u in range(nusers):
-            sname=gname+chr(u+65) # Letters A,B,C....
+            sname=gname+chr(u+97) # Letters A,B,C....
             fname=sname+'fn'
             lname=sname+'ln'
             nome=fname+" "+lname
             username=sname
             email=username+"@"+gmail[n]
-            csvwriter.writerows([[username,fname,lname,email,gpass[n],gcohort[n],fake.msisdn()]])
+            csvwriter.writerows([[username,fname,lname,email,gpass[n],gcohort[n]]])
             # ['username', 'firstname', 'lastname', 'email','password','cohort1','idnumber'] 
             
